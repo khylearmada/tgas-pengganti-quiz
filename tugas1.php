c0d85c2b745e5ea8ca61790d4f26cac88dfa22a6
khylearmada <khaylaarmada.08@gmail.com>
2025-05-12 21:49:53 +0800

    hapus

--- tugas1.php
+++ tugas1.php
@@ -1,18 +1,1 @@
-<?php
 
-echo "nama: Auliyah Nastity Khayla Armada. <br>";
-echo "tempat tanggal lahir: surabaya.14juni 2008. <br>";
-echo "status: pelajar. <br>";
-echo "biografi saya: <br>";
-echo "saya lebih senang jika seseorang memanggil saya dengan sebutan khayla. saya sekarang sedang berusia 16tahun
-yang sedang menuntut ilmu di SMKKIT ibnul qoyyim school. saya adalah murid pindahan disana saya dipindahkan oleh bunda
-saya lantaran saya mengidap suatu penyakit.namun, saya bersyukur memiliki 2orang sahabat yang sangat absurd dan amat
-sangat baik kepada saya. <br>";
-
-print "BEBERAPA TAMBAHAN. <br>";
-print "hobby: berenang, bermain gadget, ngonten yt dan bermain bola basket(jika ada waktu) membaca dan menulis cerita fiksi melukis olahraga. <br>";
-print "makanan fav: ice cream. ba'so tusuk. indomie rendang. cimory squeeze. sosis kezeler.DLL. <br>";
-print "minuman fav: air putih dan minuman dingin. <br>";
-print "<br>";
-print "<br>";
-print "SEKIAN BIOGRAFI DARI SAYA.";

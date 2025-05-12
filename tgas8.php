f06590bf307aca494852a52fb3c7f3fed5a2719b
khylearmada <khaylaarmada.08@gmail.com>
2025-05-12 21:47:54 +0800

    hapus

--- tgas8.php
+++ tgas8.php
@@ -1,10 +1,1 @@
-<?php
-$nadia_berpuasa = false;
-$nadia_tidak_puasa = true;
 
-if ($nadia_berpuasa){
-    echo "berbuka dengan bakso";
-}
-elseif ($nadia_tidak_puasa){
-    echo " makan siang dengan coto";
-}
